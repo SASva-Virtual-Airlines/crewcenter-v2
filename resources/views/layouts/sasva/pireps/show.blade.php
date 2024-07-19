c24aad9ec85d7affc79bd9b0c1bbc22786aea2ac
Benjamin Henning Bruun Jørgensen <benjamin@dunkstormen.dk>
2024-07-19 13:21:42 +0200

    Cleanup

--- resources/views/layouts/sasva/pireps/show.blade.php
+++ resources/views/layouts/sasva/pireps/show.blade.php
@@ -51,40 +51,7 @@
         </div>
       </div>
       <div class="w-full flex flex-row gap-8 mt-8">
-        <div id="airportInboundFlights" class="w-6/12 bg-white shadow-sm">
-          <div id="airportInboundFlights_head" class="p-4 border-b border-gray-100">
-            <h2 class="text-xl font-medium">Inbound flights</h2>
-            <h6 class="text-sm text-gray-500">Flights flying into</h6>
-          </div>
-          <div id="airportInboundFlights_body">
-            <table class="table-auto w-full">
-              <thead class="bg-blue-900">
-                <th class="text-base text-white font-medium px-2 py-3">Flight Number</th>
-                <th class="text-base text-white font-medium px-2 py-3">Departure Airport</th>
-              </thead>
-              <tbody class="divide-y divide-gray-100">
-                
-              </tbody>
-            </table>
-          </div>
-        </div>
-
-        <div id="airportOutboundFlights" class="w-6/12 bg-white shadow-sm">
-          <div id="airportOutboundFlights_head" class="p-4 border-b border-gray-100">
-            <h2 class="text-xl font-medium">Outbound flights</h2>
-            <h6 class="text-sm text-gray-500">Flights flying from </h6>
-          </div>
-          <div id="airportOutboundFlights_body">
-            <table class="table-auto w-full">
-              <thead class="bg-blue-900">
-                <th class="text-base text-white font-medium px-2 py-3">Flight Number</th>
-                <th class="text-base text-white font-medium px-2 py-3">Arrival Airport</th>
-              </thead>
-              <tbody class="divide-y divide-gray-100">
-              </tbody>
-            </table>
-          </div>
-        </div>
+        <!-- TODO -->
       </div>
       
     </div>
@@ -94,162 +61,12 @@
           <h2 class="text-xl font-medium">Airport notes</h2>
         </div>
         <div id="airportNotes_body" class="p-4">
-          Test
+          {{ $pirep->comments }}
         </div>
       </div>
       
     </div>
   </div>
-
-
-  <div class="row">
-    <div class="col-sm-8">
-      <h2>{{ $pirep->ident }} : {{ $pirep->dpt_airport_id }} to {{ $pirep->arr_airport_id }}</h2>
-    </div>
-
-    <div class="col-sm-4">
-      {{-- Show the link to edit if it can be edited --}}
-      @if (!empty($pirep->simbrief))
-        <a href="{{ url(route('frontend.simbrief.briefing', [$pirep->simbrief->id])) }}"
-           class="btn btn-outline-info">View SimBrief</a>
-      @endif
-
-      @if(!$pirep->read_only && $user && $pirep->user_id === $user->id)
-        <div class="float-right" style="margin-bottom: 10px;">
-          <form method="get"
-                action="{{ route('frontend.pireps.edit', $pirep->id) }}"
-                style="display: inline">
-            @csrf
-            <button class="btn btn-outline-info">@lang('common.edit')</button>
-          </form>
-          &nbsp;
-          <form method="post"
-                action="{{ route('frontend.pireps.submit', $pirep->id) }}"
-                style="display: inline">
-            @csrf
-            <button class="btn btn-outline-success">@lang('common.submit')</button>
-          </form>
-        </div>
-      @endif
-    </div>
-  </div>
-
-  <div class="row">
-    <div class="col-8">
-      <div class="row">
-        {{--
-            DEPARTURE INFO
-        --}}
-        <div class="col-6 text-left">
-          <h4>
-            {{$pirep->dpt_airport->location}}
-          </h4>
-          <p>
-            <a href="{{route('frontend.airports.show', $pirep->dpt_airport_id)}}">
-              {{ $pirep->dpt_airport->full_name }} ({{  $pirep->dpt_airport_id }})</a>
-            <br/>
-            @if($pirep->block_off_time)
-              {{ $pirep->block_off_time->toDayDateTimeString() }}
-            @endif
-          </p>
-        </div>
-
-        {{--
-            ARRIVAL INFO
-        --}}
-        <div class="col-6 text-right">
-          <h4>
-            {{$pirep->arr_airport->location}}
-          </h4>
-          <p>
-            <a href="{{route('frontend.airports.show', $pirep->arr_airport_id)}}">
-              {{ $pirep->arr_airport->full_name }} ({{  $pirep->arr_airport_id }})</a>
-            <br/>
-            @if($pirep->block_on_time)
-              {{ $pirep->block_on_time->toDayDateTimeString() }}
-            @endif
-          </p>
-        </div>
-      </div>
-
-      @if(!empty($pirep->distance))
-        <div class="row">
-          <div class="col-12">
-            <div class="progress" style="margin: 20px 0;">
-              <div class="progress-bar progress-bar-success" role="progressbar"
-                  aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"
-                  style="width: {{$pirep->progress_percent}}%;">
-              </div>
-            </div>
-          </div>
-        </div>
-      @endif
-
-    {{--
-
-    RIGHT SIDEBAR
-
-    --}}
-
-    <div class="col-4">
-      <table class="table table-striped">
-        <tr>
-          <td width="30%">@lang('common.state')</td>
-          <td>
-            <div class="badge badge-info">
-              {{ PirepState::label($pirep->state) }}
-            </div>
-          </td>
-        </tr>
-
-        @if ($pirep->state !== PirepState::DRAFT)
-        <tr>
-          <td width="30%">@lang('common.status')</td>
-          <td>
-            <div class="badge badge-info">
-              {{ PirepStatus::label($pirep->status) }}
-            </div>
-          </td>
-        </tr>
-        @endif
-
-        <tr>
-          <td>@lang('pireps.source')</td>
-          <td>{{ PirepSource::label($pirep->source) }}</td>
-        </tr>
-
-        <tr>
-          <td>@lang('flights.flighttype')</td>
-          <td>{{ \App\Models\Enums\FlightType::label($pirep->flight_type) }}</td>
-        </tr>
-
-        <tr>
-          <td>@lang('pireps.filedroute')</td>
-          <td>{{ $pirep->route }}</td>
-        </tr>
-
-        <tr>
-          <td>{{ trans_choice('common.note', 2) }}</td>
-          <td>{{ $pirep->notes }}</td>
-        </tr>
-
-        @if($pirep->score && $pirep->landing_rate)
-          <tr>
-            <td>Score</td>
-            <td>{{ $pirep->score }}</td>
-          </tr>
-          <tr>
-            <td>Landing Rate</td>
-            <td>{{ number_format($pirep->landing_rate) }}</td>
-          </tr>
-        @endif
-
-        <tr>
-          <td>@lang('pireps.filedon')</td>
-          <td>{{ show_datetime($pirep->created_at) }}</td>
-        </tr>
-
-      </table>
 
   @if(!empty($pirep->simbrief))
     <div class="separator"></div>
