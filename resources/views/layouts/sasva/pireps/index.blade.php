20cfdd6447fca829886bb8b8393baf58e6e50c09
Benjamin Henning Bruun Jørgensen <benjamin@dunkstormen.dk>
2024-07-22 00:11:21 +0200

    Updated Pirep table

--- resources/views/layouts/sasva/pireps/index.blade.php
+++ resources/views/layouts/sasva/pireps/index.blade.php
@@ -2,6 +2,49 @@
 @section('title', trans_choice('common.pirep', 2))
 
 @section('content')
+  <div id="content" class="w-full flex gap-8">
+    <div class="w-9/12 flex flex-col self-start">
+      <div id="flights" class="bg-white shadow-sm">
+        <div id="flights__head" class="p-4 border-b border-gray-100">
+          <h2 class="text-xl font-medium">{{ trans_choice('pireps.pilotreport', 2) }}</h2>
+          <h6 class="text-sm text-gray-500">Flights you've flown</h6>
+        </div>
+        <div id="flights__body">
+          <table class="table-auto w-full">
+            <thead class="bg-blue-900">
+            <th class="text-base text-white text-left font-medium px-4 py-3">Flight Number</th>
+            <th class="text-base text-white text-left font-medium px-4 py-3">Departure</th>
+            <th class="text-base text-white text-left font-medium px-4 py-3">Arrival</th>
+            <th class="text-base text-white text-center font-medium px-4 py-3">Aircraft</th>
+            <th class="text-base text-white text-center font-medium px-4 py-3">Flight Time</th>
+            <th class="text-base text-white text-center font-medium px-4 py-3">Status</th>
+            <th class="text-base text-white text-center font-medium px-4 py-3">Submitted</th>
+            </thead>
+            <tbody class="divide-y divide-gray-100">
+            @include('pireps.table')
+            </tbody>
+          </table>
+        </div>
+        <div id="flights__footer">
+          {{ $pireps->withQueryString()->links('pagination.default') }}
+        </div>
+      </div>
+    </div>
+    <div class="w-3/12 flex flex-col self-start">
+      <div id="flightSearch" class="bg-white shadow-sm">
+        <div id="flightSearch__head" class="p-4 border-b border-gray-100">
+          <h2 class="text-xl font-medium">Search</h2>
+          <h6 class="text-sm text-gray-500">Search for specific flights</h6>
+        </div>
+        <div id="flightSearch__body">
+          
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
+
+@section('keep_for_ref')
   <div class="row">
     <div class="col-md-12">
       <div style="float:right;">
@@ -9,9 +52,9 @@
            style="margin-top: -10px;margin-bottom: 5px"
            href="{{ route('frontend.pireps.create') }}">@lang('pireps.filenewpirep')</a>
       </div>
-      <h2>{{ trans_choice('pireps.pilotreport', 2) }}</h2>
+      <h2></h2>
       @include('flash::message')
-      @include('pireps.table')
+      
     </div>
   </div>
   <div class="row">
--- resources/views/layouts/sasva/pireps/table.blade.php
+++ resources/views/layouts/sasva/pireps/table.blade.php
@@ -1,73 +1,33 @@
-<div class="table-responsive">
-  <table class="table table-hover table-striped">
-    <thead>
-    <tr>
-      <th>@sortablelink('flight_number', trans_choice('common.flight', 1))</th>
-      <th>@sortablelink('dpt_airport_id', __('common.departure'))</th>
-      <th>@sortablelink('arr_airport_id', __('common.arrival'))</th>
-      <th>@sortablelink('aircraft_id', __('common.aircraft'))</th>
-      <th class="text-center">@sortablelink('flight_time', __('flights.flighttime'))</th>
-      <th class="text-center">@sortablelink('status', __('common.status'))</th>
-      <th>@sortablelink('submitted_at', __('pireps.submitted'))</th>
-      <th></th>
-    </tr>
-    </thead>
-    <tbody>
-
-    @foreach($pireps as $pirep)
-      <tr>
-        <td>
-          <a href="{{ route('frontend.pireps.show', [$pirep->id]) }}">{{ $pirep->ident }}</a>
-        </td>
-        <td>
-          @if($pirep->dpt_airport){{ $pirep->dpt_airport->name }}@endif
-                  (<a href="{{route('frontend.airports.show', [$pirep->dpt_airport_id])}}">{{$pirep->dpt_airport_id}}</a>)
-        </td>
-        <td>
-          @if($pirep->arr_airport){{ $pirep->arr_airport->name }}@endif
-                  (<a href="{{route('frontend.airports.show', [$pirep->arr_airport_id])}}">{{$pirep->arr_airport_id}}</a>)
-        </td>
-        <td>
-          @if($pirep->aircraft)
-            {{ optional($pirep->aircraft)->ident }}
-          @else
-            -
-          @endif
-        </td>
-        <td class="text-center">
-          @minutestotime($pirep->flight_time)
-        </td>
-        <td class="text-center">
-          @php
-            $color = 'badge-info';
-            if($pirep->state === PirepState::PENDING) {
-                $color = 'badge-warning';
-            } elseif ($pirep->state === PirepState::ACCEPTED) {
-                $color = 'badge-success';
-            } elseif ($pirep->state === PirepState::REJECTED) {
-                $color = 'badge-danger';
-            }
-          @endphp
-          <div class="badge {{ $color }}">{{ PirepState::label($pirep->state) }}</div>
-        </td>
-        <td>
-          @if(filled($pirep->submitted_at))
-            {{ $pirep->submitted_at->diffForHumans() }}
-          @endif
-        </td>
-        <td>
-          @if(!$pirep->read_only)
-            <a href="{{ route('frontend.pireps.edit', [$pirep->id]) }}"
-               class="btn btn-outline-info btn-sm"
-               style="z-index: 9999"
-               title="@lang('common.edit')">
-              @lang('common.edit')
-            </a>
-          @endif
-        </td>
-      </tr>
-    @endforeach
-
-    </tbody>
-  </table>
-</div>
+@foreach($pireps as $pirep)
+  <tr class="hover:bg-gray-200">
+    <td class="text-base text-left font-medium py-3 px-4">{{ $pirep->ident }}</td>
+    <td class="text-base text-center font-medium py-3 px-4">{{ $pirep->dpt_airport->name }}</td>
+    <td class="text-base text-center font-medium py-3 px-4">{{ $pirep->arr_airport->name }}</td>
+    <td class="text-base text-center font-medium py-3 px-4">
+      @if($pirep->aircraft)
+        {{ optional($pirep->aircraft)->ident }}
+      @else
+        -
+      @endif
+    </td>
+    <td class="text-base text-center font-medium py-3 px-4">@minutestotime($pirep->flight_time)</td>
+    <td class="text-base text-center font-medium py-3 px-4">
+      @php
+        $color = 'bg-blue-900';
+        if($pirep->state === PirepState::PENDING) {
+            $color = 'bg-yellow-600';
+        } elseif ($pirep->state === PirepState::ACCEPTED) {
+            $color = 'bg-green-600';
+        } elseif ($pirep->state === PirepState::REJECTED) {
+            $color = 'bg-red-600';
+        }
+      @endphp
+      <div class="{{ $color }} text-white text-xs font-medium px-2 py-1 rounded-sm">{{ PirepState::label($pirep->state) }}</div>
+    </td>
+    <td class="text-base text-center font-medium py-3 px-4">
+      @if(filled($pirep->submitted_at))
+        {{ $pirep->submitted_at->diffForHumans() }}
+      @endif
+    </td>
+  </tr>
+@endforeach
